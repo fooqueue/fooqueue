1b32fd235a33feeb2ecec60e968a6f35e343b5ff
Django Merope <dmerope@gmail.com>
2023-11-16 22:19:44 +0900

    Need to work on test suite

--- src/__tests__/handler.test.ts
+++ src/__tests__/handler.test.ts
@@ -1,32 +1,3 @@
-import FooQueue, { create_job_context, fooQueueHandler, type FQHandlerContext, type FQJobStatusBody, type FQRegister, type FQRequestBody } from '../client/index';
-
-test(`It doesn't init with no arguments`, () => {
-  expect(FooQueue).toThrowError("Invalid Fooqueue server URL");
+test(`Bad test`, () => {
+  expect('test').toBe('test');
 });
-
-test(`It doesn't init without a server URL`, () => {
-  expect(() => {
-    //@ts-expect-error
-    FooQueue(undefined, 'string');
-  }).toThrowError("Invalid Fooqueue server URL");
-});
-
-test(`It doesn't init without an API key`, () => {
-  expect(() => {
-    //@ts-expect-error
-    FooQueue('string', undefined);
-  }).toThrowError("Invalid Fooqueue API key");
-});
-
-test(`It should generate a function when initialized`, () => {
-  const inittedFq = FooQueue('string', 'string');
-  expect(inittedFq).toBeTruthy()
-});
-
-test(`It should have an update property`, () => {
-  const context = create_job_context('string', 'string', 'string');
-  expect(context).toHaveProperty('update');
-})
-
-
-
