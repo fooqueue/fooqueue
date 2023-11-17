11ac872691a1fab18329259d598cce5a2b2bdda0
Django Merope <dmerope@gmail.com>
2023-11-17 12:38:06 +0900

    removed default export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import {enqueue} from './client/lib/enqueue';
-import {FooqueueSveltekitHandler} from './client/lib/sveltekit_handler';
+export {enqueue} from './client/lib/enqueue';
+export {FooqueueSveltekitHandler} from './client/lib/sveltekit_handler';
 export type {EnqueueOptions} from './client/types/EnqueueOptions';
-
-export default {
-  enqueue, FooqueueSveltekitHandler
-};
