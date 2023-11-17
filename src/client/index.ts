da0d86c38c1a7628866b3645af0691c5d5fc1553
Django Merope <dmerope@gmail.com>
2023-11-17 11:16:31 +0900

    moved to base src directory

--- src/client/index.ts
+++
@@ -1,7 +0,0 @@
-
-
-import fooqueue from './lib/enqueue';
-const default_object = Object.assign({}, fooqueue);
-export default default_object;
-export * from './lib/sveltekit_handler';
-export * from './types/EnqueueOptions';
