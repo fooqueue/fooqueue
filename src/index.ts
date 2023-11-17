c3163a0eff306aa8d9634d31da3651f226f9dd03
Django Merope <dmerope@gmail.com>
2023-11-17 12:14:27 +0900

    updated commonjs handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import fooqueue from './client/lib/enqueue';
-const default_object = Object.assign({}, fooqueue);
-export default default_object;
-export * from './client/lib/sveltekit_handler';
-export * from './client/types/EnqueueOptions';
+import {default as enqueue} from './client/lib/enqueue';
+import {FooqueueSveltekitHandler} from './client/lib/sveltekit_handler';
+export type {EnqueueOptions} from './client/types/EnqueueOptions';
+
+export default {
+  enqueue, FooqueueSveltekitHandler
+}
